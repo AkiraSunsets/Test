e7af8b92f51d8f055f4ca95fc3d2621b79243b60
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2026-02-23 16:58:08 -0300

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+/*import "./footer.css" */
+
+export default function Footer() {
+  return (
+    <footer className="footer" id="contato">
+      <div className="container footer__inner">
+        <p>©2026 - Marymar — Mini projeto responsivo</p>
+        <a href="#sobre">Voltar ao topo</a>
+      </div>
+    </footer>
+  );
+}
+
